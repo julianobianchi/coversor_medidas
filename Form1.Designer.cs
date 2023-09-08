2eacc4ee5e876a53d701bf42b6cecfdd5c024452
Juliano Bianchi <Juliano Bianchi@DESKTOP-IK3G9K1>
2023-09-08 20:17:57 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace coversor_medidas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn7 = new Button();
+            btn8 = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
+            button10 = new Button();
+            btn0 = new Button();
+            btnLimpar = new Button();
+            btnVoltar = new Button();
+            lblRes1 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            lblRes2 = new Label();
+            btn9 = new Button();
+            label1 = new Label();
+            btnEnter = new Button();
+            SuspendLayout();
+            // 
+            // btn7
+            // 
+            btn7.BackColor = SystemColors.MenuBar;
+            btn7.BackgroundImageLayout = ImageLayout.Center;
+            btn7.FlatStyle = FlatStyle.Popup;
+            btn7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn7.Location = new Point(17, 256);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(68, 44);
+            btn7.TabIndex = 0;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = SystemColors.MenuBar;
+            btn8.BackgroundImageLayout = ImageLayout.Center;
+            btn8.FlatStyle = FlatStyle.Popup;
+            btn8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn8.Location = new Point(91, 256);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(68, 44);
+            btn8.TabIndex = 1;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn6
+            // 
+            btn6.BackColor = SystemColors.MenuBar;
+            btn6.BackgroundImageLayout = ImageLayout.Center;
+            btn6.FlatStyle = FlatStyle.Popup;
+            btn6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn6.Location = new Point(165, 306);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(68, 44);
+            btn6.TabIndex = 5;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btn5
+            // 
+            btn5.BackColor = SystemColors.MenuBar;
+            btn5.BackgroundImageLayout = ImageLayout.Center;
+            btn5.FlatStyle = FlatStyle.Popup;
+            btn5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn5.Location = new Point(91, 306);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(68, 44);
+            btn5.TabIndex = 4;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn4
+            // 
+            btn4.BackColor = SystemColors.MenuBar;
+            btn4.BackgroundImageLayout = ImageLayout.Center;
+            btn4.FlatStyle = FlatStyle.Popup;
+            btn4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn4.Location = new Point(17, 306);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(68, 44);
+            btn4.TabIndex = 3;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btn3
+            // 
+            btn3.BackColor = SystemColors.MenuBar;
+            btn3.BackgroundImageLayout = ImageLayout.Center;
+            btn3.FlatStyle = FlatStyle.Popup;
+            btn3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn3.Location = new Point(165, 356);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(68, 44);
+            btn3.TabIndex = 8;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn2
+            // 
+            btn2.BackColor = SystemColors.MenuBar;
+            btn2.BackgroundImageLayout = ImageLayout.Center;
+            btn2.FlatStyle = FlatStyle.Popup;
+            btn2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn2.Location = new Point(91, 356);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(68, 44);
+            btn2.TabIndex = 7;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn1
+            // 
+            btn1.BackColor = SystemColors.MenuBar;
+            btn1.BackgroundImageLayout = ImageLayout.Center;
+            btn1.FlatStyle = FlatStyle.Popup;
+            btn1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn1.Location = new Point(17, 356);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(68, 44);
+            btn1.TabIndex = 6;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.MenuBar;
+            button10.BackgroundImageLayout = ImageLayout.Center;
+            button10.FlatStyle = FlatStyle.Popup;
+            button10.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button10.Location = new Point(164, 406);
+            button10.Name = "button10";
+            button10.Size = new Size(68, 44);
+            button10.TabIndex = 11;
+            button10.Text = ",";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // btn0
+            // 
+            btn0.BackColor = SystemColors.MenuBar;
+            btn0.BackgroundImageLayout = ImageLayout.Center;
+            btn0.FlatStyle = FlatStyle.Popup;
+            btn0.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn0.Location = new Point(90, 406);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(68, 44);
+            btn0.TabIndex = 10;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btnLimpar
+            // 
+            btnLimpar.BackColor = SystemColors.MenuBar;
+            btnLimpar.BackgroundImageLayout = ImageLayout.Center;
+            btnLimpar.FlatStyle = FlatStyle.Popup;
+            btnLimpar.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnLimpar.Location = new Point(238, 256);
+            btnLimpar.Name = "btnLimpar";
+            btnLimpar.Size = new Size(68, 44);
+            btnLimpar.TabIndex = 9;
+            btnLimpar.Text = "CE";
+            btnLimpar.UseVisualStyleBackColor = true;
+            btnLimpar.Click += btnLimpar_Click;
+            // 
+            // btnVoltar
+            // 
+            btnVoltar.BackColor = SystemColors.MenuBar;
+            btnVoltar.BackgroundImageLayout = ImageLayout.Center;
+            btnVoltar.FlatStyle = FlatStyle.Popup;
+            btnVoltar.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnVoltar.Location = new Point(239, 306);
+            btnVoltar.Name = "btnVoltar";
+            btnVoltar.Size = new Size(67, 44);
+            btnVoltar.TabIndex = 12;
+            btnVoltar.Text = "<=";
+            btnVoltar.UseVisualStyleBackColor = true;
+            btnVoltar.Click += btnVoltar_Click;
+            // 
+            // lblRes1
+            // 
+            lblRes1.AutoSize = true;
+            lblRes1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            lblRes1.Location = new Point(25, 81);
+            lblRes1.Name = "lblRes1";
+            lblRes1.Size = new Size(0, 40);
+            lblRes1.TabIndex = 15;
+            lblRes1.UseCompatibleTextRendering = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Milímetro", "Centímetro", "Metros", "Quilômetros", "Polegadas", "Pés" });
+            comboBox1.Location = new Point(25, 121);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(280, 23);
+            comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Milímetro", "Centímetro", "Metros", "Quilômetros", "Polegadas", "Pés" });
+            comboBox2.Location = new Point(25, 188);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(280, 23);
+            comboBox2.TabIndex = 16;
+            // 
+            // lblRes2
+            // 
+            lblRes2.AutoSize = true;
+            lblRes2.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            lblRes2.Location = new Point(25, 148);
+            lblRes2.Name = "lblRes2";
+            lblRes2.Size = new Size(24, 42);
+            lblRes2.TabIndex = 15;
+            lblRes2.Text = "0";
+            lblRes2.UseCompatibleTextRendering = true;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = SystemColors.MenuBar;
+            btn9.BackgroundImageLayout = ImageLayout.Center;
+            btn9.FlatStyle = FlatStyle.Popup;
+            btn9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btn9.Location = new Point(164, 256);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(68, 44);
+            btn9.TabIndex = 17;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(52, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(222, 28);
+            label1.TabIndex = 18;
+            label1.Text = "Conversor de Medidas";
+            // 
+            // btnEnter
+            // 
+            btnEnter.BackColor = SystemColors.MenuBar;
+            btnEnter.BackgroundImageLayout = ImageLayout.Center;
+            btnEnter.FlatStyle = FlatStyle.Popup;
+            btnEnter.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnEnter.Location = new Point(238, 356);
+            btnEnter.Name = "btnEnter";
+            btnEnter.Size = new Size(67, 94);
+            btnEnter.TabIndex = 19;
+            btnEnter.Text = "Enter";
+            btnEnter.UseVisualStyleBackColor = true;
+            btnEnter.Click += btnEnter_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(320, 457);
+            Controls.Add(btnEnter);
+            Controls.Add(label1);
+            Controls.Add(btn9);
+            Controls.Add(comboBox2);
+            Controls.Add(lblRes2);
+            Controls.Add(comboBox1);
+            Controls.Add(lblRes1);
+            Controls.Add(btnVoltar);
+            Controls.Add(button10);
+            Controls.Add(btn0);
+            Controls.Add(btnLimpar);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn7;
+        private Button btn8;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
+        private Button button10;
+        private Button btn0;
+        private Button btnLimpar;
+        private Button btnVoltar;
+        private Label lblRes1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label lblRes2;
+        private Button btn9;
+        private Label label1;
+        private Button btnEnter;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,412 @@
+namespace coversor_medidas
+{
+    public partial class Form1 : Form
+    {
+        public double num0;
+        public double num1;
+        public double num2;
+        public double num3;
+        public double num4;
+        public double num5;
+        public double num6;
+        public double num7;
+        public double num8;
+        public double num9;
+        public double virgula;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 0;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 1;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 2;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 3;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 4;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 5;
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 6;
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 7;
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 8;
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += 9;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text = string.Empty;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text += ",";
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            lblRes1.Text = string.Empty;
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            //calulos
+            if (comboBox1.Text == "Metros" && comboBox2.Text == "Centímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 100;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Metros" && comboBox2.Text == "Milímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 1000;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Metros" && comboBox2.Text == "Quilômetros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 1000;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Metros" && comboBox2.Text == "Polegadas")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 39.37;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Metros" && comboBox2.Text == "Pés")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 3.28084;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Metros" && comboBox2.Text == "Metros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                lblRes2.Text = num.ToString();
+            }
+            if (comboBox1.Text == "Centímetro" && comboBox2.Text == "Metros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 100;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Centímetro" && comboBox2.Text == "Milímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 10;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Centímetro" && comboBox2.Text == "Quilômetros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 100000;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Centímetro" && comboBox2.Text == "Polegadas")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 0.39;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Centímetro" && comboBox2.Text == "Pés")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 0.032808;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Centímetro" && comboBox2.Text == "Centímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                lblRes2.Text = num.ToString();
+            }
+            if (comboBox1.Text == "Milímetro" && comboBox2.Text == "Metros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 1000;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Milímetro" && comboBox2.Text == "Centímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 10;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Milímetro" && comboBox2.Text == "Quilômetros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 0.000001;
+
+                lblRes2.Text = total.ToString("0.000000");
+            }
+            if (comboBox1.Text == "Milímetro" && comboBox2.Text == "Polegadas")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 25.4;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Milímetro" && comboBox2.Text == "Pés")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 304.8;
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Milímetro" && comboBox2.Text == "Milímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                lblRes2.Text = num.ToString();
+            }
+            if (comboBox1.Text == "Quilômetros" && comboBox2.Text == "Centímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 100000;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Quilômetros" && comboBox2.Text == "Milímetro")
+            {
+                Double total;
+                Double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 1000000;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Quilômetros" && comboBox2.Text == "Metros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 1000;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Quilômetros" && comboBox2.Text == "Polegadas")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 39370;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Quilômetros" && comboBox2.Text == "Pés")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 3280.84;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Quilômetros" && comboBox2.Text == "Quilômetros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                lblRes2.Text = num.ToString();
+            }
+            //Falta concluir
+
+
+
+
+
+            if (comboBox1.Text == "Polegadas" && comboBox2.Text == "Centímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 2.45;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Polegadas" && comboBox2.Text == "Milímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 25.4;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Polegadas" && comboBox2.Text == "Quilômetros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 0.0000254;
+
+                lblRes2.Text = total.ToString("0.0000000");
+            }
+            if (comboBox1.Text == "Polegadas" && comboBox2.Text == "Metros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 39.37;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Polegadas" && comboBox2.Text == "Pés")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 12;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Polegadas" && comboBox2.Text == "Polegadas")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                lblRes2.Text = num.ToString();
+            }
+
+            if (comboBox1.Text == "Pés" && comboBox2.Text == "Centímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 30.48;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Pés" && comboBox2.Text == "Milímetro")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 304.8;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Pés" && comboBox2.Text == "Quilômetros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 3281;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Pés" && comboBox2.Text == "Metros")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num / 3.281;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Pés" && comboBox2.Text == "Polegadas")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                total = num * 12;
+
+                lblRes2.Text = total.ToString();
+            }
+            if (comboBox1.Text == "Pés" && comboBox2.Text == "Pés")
+            {
+                double total;
+                double num;
+                num = Convert.ToDouble(lblRes1.Text);
+                lblRes2.Text = num.ToString();
+            }
+        }
+    }
+}
+//num1 = Convert.ToDouble(txtNum1.Text);
+//txtResul.Text = resultado.ToString();
